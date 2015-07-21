36f93ea648b2363a97cf89a60624fef919d352f8
Chris Chen <chenstopher@gmail.com>
2015-07-20 23:34:46 -0400

    Testing out physics

--- depth/DepthGame.js
+++ depth/DepthGame.js
@@ -12,6 +12,8 @@ depth.DepthGame = function(options){
 	this.scene = null;
 	this.camera = null;
 	this.cube = null;
+
+	this.ground = null;
 };
 
 depth.DepthGame.prototype.init = function(){
@@ -35,31 +37,39 @@ depth.DepthGame.prototype.init = function(){
 	this.scene = new BABYLON.Scene(this.engine);
 	this.scene.clearColor = new BABYLON.Color3(0,0,0);
 	this.scene.ambientColor = new BABYLON.Color3(1,1,1);
-
-	this.camera = new BABYLON.TargetCamera("Camera", BABYLON.Vector3.Zero(), this.scene);
-	this.camera.attachControl(this.canvas, false);
-
-	this.light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(.1, 1, .1), this.scene);
-	this.light.intensity = 0.5;
-	this.light.diffuse = new BABYLON.Color3(1, 1, 1);
-	this.light.specular = new BABYLON.Color3(1, 1, 1);
-	this.light.groundColor = new BABYLON.Color3(0, 0, 0);
-
-	this.cube = new BABYLON.Mesh.CreateBox("box", 1, this.scene);
-	this.cube.position.z = 10;
+	this.scene.enablePhysics(new BABYLON.Vector3(0, -9.8, 0), new BABYLON.OimoJSPlugin());
 
 	// skybox
-	var skybox = BABYLON.Mesh.CreateBox("skybox", 100.0, this.scene);
+	var skybox = BABYLON.Mesh.CreateBox("skybox", 10000.0, this.scene);
 	var skyboxMat = new BABYLON.StandardMaterial("skybox", this.scene);
 	skyboxMat.backFaceCulling = false;
 	skyboxMat.reflectionTexture = new BABYLON.CubeTexture("textures/skybox/skybox", this.scene, ["_px.png", "_py.png", "_pz.png", "_nx.png", "_ny.png", "_nz.png"]);
 	skyboxMat.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+	skyboxMat.reflectionTexture.samplingMode = BABYLON.Texture.NEAREST_SAMPLINGMODE;
+	skyboxMat.reflectionTexture.anisotropicFilteringLevel = 1;
 	skyboxMat.diffuseColor = new BABYLON.Color3(0, 0, 0);
 	skyboxMat.specularColor = new BABYLON.Color3(0, 0, 0);
 	skybox.material = skyboxMat;
 	skybox.infinteDistance = true;
 	skybox.renderingGroupId = 0;
 
+	this.camera = new BABYLON.TargetCamera("Camera", BABYLON.Vector3.Zero(), this.scene);
+	this.camera.attachControl(this.canvas, false);
+
+	this.light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(.1, 1, .1), this.scene);
+	this.light.intensity = 0.8;
+	this.light.diffuse = new BABYLON.Color3(1, 1, 1);
+	this.light.specular = new BABYLON.Color3(1, 1, 1);
+	this.light.groundColor = new BABYLON.Color3(0, 0, 0);
+
+	this.cube = new BABYLON.Mesh.CreateBox("box", 1, this.scene);
+	this.cube.position.z = 10;
+	this.cube.setPhysicsState(BABYLON.PhysicsEngine.BoxImpostor, {mass: 1});
+
+	this.ground = new BABYLON.Mesh.CreateBox("ground", 100, this.scene, false, BABYLON.Mesh.DOUBLESIDE);
+	this.ground.rotation.x = BABYLON.Tools.ToRadians(-90);
+	this.ground.position.y = -53;
+	this.ground.setPhysicsState(BABYLON.PhysicsEngine.BoxImpostor, {move: false});
 };
 
 depth.DepthGame.prototype.start = function(){
@@ -113,7 +123,6 @@ depth.DepthGame.prototype.update = function(){
 		this.camera.cameraRotation.x = Input.getMouseDelta().y * sensitivity;
 		this.camera.cameraRotation.y = Input.getMouseDelta().x * sensitivity;
 	}
-
 
 	Input.endFrame();
 };
---
+++ depth/Player.js
@@ -0,0 +1,11 @@
+var depth = depth || {};
+
+depth.Player = function(){
+};
+
+depth.Player.prototype.init = function(){
+};
+
+depth.Player.prototype.update = function(deltaTime){
+};
+
